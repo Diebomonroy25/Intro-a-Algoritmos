5aa810be3b8dd92eaf6f7b48a13db77347d1bcb1
Diego <2109110@upy.edu.mx>
2022-02-24 15:39:53 -0600

    Se agregaron dos programas, uno que es explicacion de los bucles anidados y otro donde se imprimen los días que tienen los meses

--- librerias/utils.h
+++ librerias/utils.h
@@ -32,7 +32,7 @@ void addUser(user** userList, user* aUserToadd){
 }
 
 void sleep(size_t m_s){  
-    m_s = m_s/1000;
+    m_s = m_s / 1;
     clock_t initial_time = time(NULL); //Tiempo inicial//
     clock_t desired_time = initial_time + m_s ;
     for(clock_t current_time = initial_time; current_time < desired_time; current_time = time(NULL) ){};
